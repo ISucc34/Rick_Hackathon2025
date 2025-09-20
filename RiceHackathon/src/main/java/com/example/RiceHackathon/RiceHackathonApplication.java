9ccaae025472f23fb7d88cdc7fda56bddfed66ee
snowcode132 <164957185+snowcode132@users.noreply.github.com>
2025-09-20 15:28:06 -0500

    Added spring boot

---
+++ RiceHackathon/src/main/java/com/example/RiceHackathon/RiceHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.RiceHackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiceHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiceHackathonApplication.class, args);
+	}
+
+}
