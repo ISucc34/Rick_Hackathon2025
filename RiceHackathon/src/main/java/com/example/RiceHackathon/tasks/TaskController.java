7dbdd1b20942cc253a58ebd746270f2456ce8426
snowcode132 <164957185+snowcode132@users.noreply.github.com>
2025-09-20 18:34:48 -0500

    Added ability to add tasks

--- RiceHackathon/src/main/java/com/example/RiceHackathon/tasks/TaskController.java
+++ RiceHackathon/src/main/java/com/example/RiceHackathon/tasks/TaskController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("tasks/api")
 public class TaskController {
